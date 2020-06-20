2060392728db93ecd8aae47f431740b954e64b62
Andrew Mullen <michalmullen@gmail.com>
2020-06-20 19:29:30 +0200

    login form

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -11,17 +11,6 @@
 				<v-icon>mdi-playlist-check</v-icon>
 			</v-btn>
 		</v-bottom-navigation>
-		<v-bottom-navigation v-if="!login" mandatory app grow color="secondary">
-			<v-btn to="/login">
-				<span>Login</span>
-				<v-icon>mdi-account</v-icon>
-			</v-btn>
-
-			<v-btn to="register">
-				<span>Register</span>
-				<v-icon>mdi-account-plus</v-icon>
-			</v-btn>
-		</v-bottom-navigation>
 	</div>
 </template>
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,54 +1,12 @@
 <template>
-	<!-- <v-container grid-list-xs>
-		<v-row dense>
-			<v-col cols="12">
-				<v-card class="elevation-12">
-					<v-toolbar dark flat>
-						<v-toolbar-title>Login form</v-toolbar-title>
-						<v-spacer></v-spacer>
-						<v-tooltip bottom>
-							<template v-slot:activator="{ on }">
-								<v-btn :href="source" icon large target="_blank" v-on="on">
-									<v-icon>mdi-code-tags</v-icon>
-								</v-btn>
-							</template>
-							<span>Source</span>
-						</v-tooltip>
-					</v-toolbar>
-					<v-card-text>
-						<v-form>
-							<v-text-field
-								label="Login"
-								name="login"
-								prepend-icon="mdi-account"
-								type="text"
-							></v-text-field>
-
-							<v-text-field
-								id="password"
-								label="Password"
-								name="password"
-								prepend-icon="mdi-lock"
-								type="password"
-							></v-text-field>
-						</v-form>
-					</v-card-text>
-					<v-card-actions>
-						<v-spacer></v-spacer>
-						<v-btn>Login</v-btn>
-					</v-card-actions>
-				</v-card>
-			</v-col>
-		</v-row>
-	</v-container> -->
 	<v-app id="inspire">
 		<v-content>
-			<v-container class="fill-height">
-				<v-row align="center" justify="center" absolute>
+			<v-container class="fill-height" fixed>
+				<v-row align="center" justify="center" absolute fixed>
 					<v-col cols="12" sm="8" md="4">
 						<v-card class="elevation-12">
 							<v-toolbar dark flat>
-								<v-toolbar-title>Login form</v-toolbar-title>
+								<v-toolbar-title>Login Form</v-toolbar-title>
 								<v-spacer></v-spacer>
 								<v-tooltip bottom>
 									<template v-slot:activator="{ on }">
@@ -79,7 +37,8 @@
 							</v-card-text>
 							<v-card-actions>
 								<v-spacer></v-spacer>
-								<v-btn>Login</v-btn>
+								<v-btn>Register</v-btn>
+								<v-btn color="secondary">Login</v-btn>
 							</v-card-actions>
 						</v-card>
 					</v-col>
